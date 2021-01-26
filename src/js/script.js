68e480a628fd84afd9be01a0e8e90153bd75321a
Kyrylo Tsybulskyi <k.tsybulskyi@gmail.com>
2021-01-26 12:40:42 +0100

    Corrected initActions. Doesn't work yet

--- src/js/script.js
+++ src/js/script.js
@@ -284,7 +284,7 @@
 
       thisWidget.element = element;
       thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.inkDecrease);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
 
     }
